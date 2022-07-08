3030b6d084a69596d352c668d84a20dac455adbe
Mohammed Ali Davami <69807363+moadav@users.noreply.github.com>
2022-07-08 19:22:00 +0200

    created weather search component and added to navigation

---
+++ ClientApp/src/components/WeatherSearch.js
@@ -0,0 +1,24 @@
+﻿import React, { Component } from 'react';
+
+export class WeatherSearch extends Component {
+    static displayName = WeatherSearch.name;
+
+    constructor(props) {
+        super(props);
+       
+    }
+
+   
+
+    render() {
+        return (
+            <div>
+                <h1>Hello Weather Search</h1>
+
+               
+
+               
+            </div>
+        );
+    }
+}
