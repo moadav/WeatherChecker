674a1dde4d4e473c6e57d69beea9804a91db24f3
Mohammed Ali Davami <69807363+moadav@users.noreply.github.com>
2022-07-08 22:33:21 +0200

    created components to create contentItems to the application

---
+++ ClientApp/src/components/ContentItem.js
@@ -0,0 +1,17 @@
+﻿import React, { Component } from 'react';
+import './ContentItem.css';
+
+export class ContentItem extends Component {
+    static displayName = ContentItem.name;
+
+
+    constructor(props) {
+        super(props);
+        
+    }
+    render() {
+        return (
+            <p> {this.props.name } </p>
+    );
+    }
+}
---
+++ ClientApp/src/components/ContentItems.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+import { ContentItem } from './ContentItem';
+
+export class ContentItems extends Component {
+    static displayName = ContentItems.name;
+
+    render() {
+        return (
+            <ContentItem name="Check Weather" />
+
+
+        );
+    }
+}
