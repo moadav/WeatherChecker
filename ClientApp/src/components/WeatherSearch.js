545ef154626df1272d6a49410b49d16fad4eaf9c
Mohammed Ali Davami <69807363+moadav@users.noreply.github.com>
2022-07-13 15:34:54 +0200

    Fixed error message.

--- ClientApp/src/components/WeatherSearch.js
+++ ClientApp/src/components/WeatherSearch.js
@@ -1,19 +1,25 @@
-﻿import React, { Component, useState } from 'react';
+﻿import React, { Component, useState, useCallback } from 'react';
 import { WeatherContentBox } from './WeatherContentBox';
 import './WeatherSearch.css';
 
 const Search = () => {
     const [municipality, setMunicipality] = useState("");
+    const [error, setError] = useState("Please enter a municipality");
     const [values, setValues] = useState();
 
     const handleChange = (e) => {
 
         setMunicipality(e.target.value)
 
+
     }
     const handleSubmit = (event) => {
         event.preventDefault();
         GetSearchApiResults();
+
+
+
+
     }
 
     const request = {
@@ -27,30 +33,39 @@ const Search = () => {
 
                 response.json()
 
-        ).then(data => setValues(data[0][0]));
+            ).then(data => {
+                setValues(data[0][0]);
+                if (values)
+                    setMunicipality(municipality);
+                setError("Could not find Municipality by name: " + municipality);
+
+            }
+
+            );
     }
 
 
     return (
         <div>
-          
-           
+
+
 
             <form className="SubmitForm" onSubmit={handleSubmit}>
- 
                 <label>
-                   KommuneNavn:
-                    <input type="text"  value={municipality} onChange={handleChange} />
+                    KommuneNavn:
+                    <input type="text" value={municipality} onChange={handleChange} />
                 </label>
                 <input type="submit" value="Submit" />
             </form>
+
             {values ? <WeatherContentBox time={values.time} air_temperature={values.air_temperature} wind_from_direction={values.wind_from_direction} wind_speed={values.wind_speed} symbol_code={values.symbol_code} />
                 :
-                null}
-            
+                <p> {error} </p>}
+
+
         </div>
 
-         
+
     );
 
 };
